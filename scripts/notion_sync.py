1c7adc5395f28fc9b015f7e1b811d0a7584337fc
Whale2357 <dydwns01234@kyonggi.ac.kr>
2026-06-23 14:54:17 +0900

    initial commit

---
+++ scripts/notion_sync.py
@@ -0,0 +1,74 @@
+import os
+import requests
+from datetime import date
+
+NOTION_TOKEN = os.environ["NOTION_TOKEN"]
+DATABASE_ID = os.environ["NOTION_DATABASE_ID"]
+
+FILE_PATH = os.environ["FILE_PATH"]
+
+parts = FILE_PATH.split("/")
+
+level = parts[1].upper()
+filename = parts[-1]
+
+problem_name = os.path.splitext(filename)[0]
+problem_name = problem_name.replace("_", " ")
+
+headers = {
+"Authorization": f"Bearer {NOTION_TOKEN}",
+"Content-Type": "application/json",
+"Notion-Version": "2022-06-28",
+}
+
+github_url = (
+f"https://github.com/{os.environ['GITHUB_REPOSITORY']}/blob/main/{FILE_PATH}"
+)
+
+payload = {
+"parent": {"database_id": DATABASE_ID},
+"properties": {
+"문제명": {
+"title": [
+{
+"text": {
+"content": problem_name
+}
+}
+]
+},
+"플랫폼": {
+"select": {
+"name": "Programmers"
+}
+},
+"난이도": {
+"select": {
+"name": level
+}
+},
+"풀이 날짜": {
+"date": {
+"start": str(date.today())
+}
+},
+"GitHub": {
+"url": github_url
+},
+"재풀이 필요": {
+"checkbox": False
+},
+"메모": {
+"rich_text": []
+}
+}
+}
+
+response = requests.post(
+"https://api.notion.com/v1/pages",
+headers=headers,
+json=payload
+)
+
+print(response.status_code)
+print(response.text)
