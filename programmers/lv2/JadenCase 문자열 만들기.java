3485f7a40103cb075798b1bed750699099c7b590
Whale2357 <dydwns01234@kyonggi.ac.kr>
2026-06-23 15:59:16 +0900

    solve

---
+++ programmers/lv2/JadenCase 문자열 만들기.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String solution(String s) {
+        StringBuilder sb = new StringBuilder();
+        boolean isNextUpper = true; // 첫 글자는 무조건 대문자 대상
+
+        for (char c : s.toCharArray()) {
+            if (c == ' ') {
+                sb.append(c);
+                isNextUpper = true; // 공백을 만났으므로 다음 글자는 대문자 후보
+            } else {
+                if (isNextUpper) {
+                    sb.append(Character.toUpperCase(c));
+                    isNextUpper = false;
+                } else {
+                    // 문제 조건에 따라 나머지 글자를 소문자로 바꿔야 한다면 toLowerCase() 사용
+                    sb.append(Character.toLowerCase(c)); 
+                }
+            }
+        }
+        return sb.toString();
+    }
+}
