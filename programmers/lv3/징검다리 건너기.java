71e2861944beb288dd57e781c97901926f5ce058
Whale2357 <dydwns01234@kyonggi.ac.kr>
2026-08-04 15:56:34 +0900

    solve: 징검다리 건너기

---
+++ programmers/lv3/징검다리 건너기.java
@@ -0,0 +1,26 @@
+import java.util.*
+
+class Solution {
+    public int solution(int[] stones, int k) {
+        Deque<Integer> deque = new ArrayDeque<>();
+        int minMax = Integer.MAX_VALUE;
+
+        for (int i = 0; i < stones.length; i++) {
+            if (!deque.isEmpty() && deque.peekFirst() < i - k + 1) {
+                deque.pollFirst();
+            }
+
+            while (!deque.isEmpty() && stones[deque.peekLast()] <= stones[i]) {
+                deque.pollLast();
+            }
+
+            deque.addLast(i);
+
+            if (i >= k - 1) {
+                minMax = Math.min(minMax, stones[deque.peekFirst()]);
+            }
+        }
+
+        return minMax;
+    }
+}
