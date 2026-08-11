6fb11a2f8bd4f093d634a3150f18bd744dd87a7f
Whale2357 <dydwns01234@kyonggi.ac.kr>
2026-08-11 15:59:14 +0900

    solve: 입국심사

---
+++ programmers/lv3/입국심사.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        long left = 1;
+        
+        long right = 0;
+        for(int time: times){
+            right = Math.max(right, time);
+        }
+        right *= (long) n;
+        
+        long answer = right;
+        
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long total = 0;
+
+            for(int time : times) {
+                total += mid / time;
+            }
+            
+            if (total >= n) {
+                answer = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return answer;
+    }
+}
